ae3037870ac3772bff22b0e883905186f6b02294
Biagio Arobba <biagio@ch-labs.com>
2017-01-10 06:19:37 +0000

    Reworked

---
+++ olckb_characters.example.php
@@ -0,0 +1,24 @@
+<?php
+
+$olckb_characters = array(
+
+    array('type' => 'CharacterGroup', 'characters' => array('a', 'b', 'c')),    
+    array('type' => 'Divider'),
+    array('type' => 'CharacterGroup', 'characters' => array('A', 'B', 'C')),
+    array('type' => 'Divider'),
+    array('type' => 'CharacterGroup', 'characters' => array('Characters can be grouped for usability')),
+    array('type' => 'Divider'),
+    array('type' => 'CharacterGroup', 'characters' => array(
+        array(
+          'show' => 'Insert: X',
+          'insert' => 'X',
+          'classes' => array('my-class')
+        )
+    )),
+
+    //array('type' => 'LineBreak'),
+    //array('type' => 'Divider'),
+    //array('type' => 'CharacterGroup', 'characters' => array('Second line')),
+    
+);
+
